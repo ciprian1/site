f599721a8cfd77a4ba226475e51166fa3a07e7ae
MCTATech <140525533+MCTATech@users.noreply.github.com>
2023-07-31 10:26:47 -0400

    Update contact.js

--- contact.js
+++ contact.js
@@ -1,3 +1,49 @@
+const captchaTextBox = document.querySelector(".captch_box input");
+const captchaInputBox = document.querySelector(".captch_input input");
+const message = document.querySelector(".message");
+const submitButton = document.querySelector("button");
+
+let captchaText = null
+
+const generateCaptcha = () => {
+    const randomString = Math.random().toString(36).substring(2, 7);
+    const randomStringArray = randomString.split("");
+    const changeString = randomStringArray.map((char) => (Math.random() > 0.5 ? char.toUpperCase() : char));
+    captchaText = changeString.join("   ");
+    captchaTextBox.value = captchaText;
+};
+
+const captchaKeyUpValidate = () => {
+    submitButton.classList.toggle("disabled", !captchaInputBox.value);
+};
+
+generateCaptcha(); 
+captchaInputBox.value = "";
+captchaKeyUpValidate();
+
+const handleCorrectCaptcha = () => {
+    document.querySelector(".contactclass").style.display = "flex";
+    document.querySelector(".containerr").style.display = "none";
+};
+
+const submitBtnClick = () => {
+    captchaText = captchaText.split("").filter((char) => char !== " ").join("");
+    message.classList.add("active");
+    if(captchaInputBox.value === captchaText){
+        message.innerText = "Captcha is correct";
+        handleCorrectCaptcha();
+    }else {
+        message.innerText = "Captcha is incorrect, refresh page";
+    }
+}
+
+captchaInputBox.addEventListener("keyup", captchaKeyUpValidate);
+submitButton.addEventListener("click", submitBtnClick);
+
+
+/*------Form Submission-------*/
+
+
 const fname = document.getElementById('fname');
 const fcompany = document.getElementById('fcompany');
 const femail = document.getElementById('femail');
@@ -23,7 +69,7 @@ submit.addEventListener('submit', (e)=>{
     Email.send({
         SecureToken : "374d6f26-5e69-43f3-b89b-4f92fd5b00b4",
         To : 'chip.maftei@gmail.com',
-        From : "tudor.alex.3000@gmail.com",
+        From : "mctatechnologies@outlook.com",
         Subject : "MCTA Form Submission from " + femail.value,
         Body : ebody
     }).then(function() {
