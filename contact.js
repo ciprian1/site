0be7f03765dfcc59b583c7fe366336149bb6a5f1
MCTATech <140525533+MCTATech@users.noreply.github.com>
2023-07-31 18:09:26 -0400

    Update contact.js

--- contact.js
+++ contact.js
@@ -69,7 +69,7 @@ submit.addEventListener('submit', (e)=>{
     Email.send({
         SecureToken : "374d6f26-5e69-43f3-b89b-4f92fd5b00b4",
         To : 'chip.maftei@gmail.com',
-        From : "mctatechnologies@outlook.com",
+        From : "office@mcta-tech.com",
         Subject : "MCTA Form Submission from " + femail.value,
         Body : ebody
     }).then(function() {
